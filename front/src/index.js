74b72df6a84b84777a8d715ac8fd2979d114e324
Inosuke <diego-dev1997@hotmail.com>
2021-08-13 19:18:56 -0500

    initial comit

---
+++ front/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HomePage } from './components/HomePage';
+import './styles/styles.scss'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
