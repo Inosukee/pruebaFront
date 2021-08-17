3eeadc3dcfb156021a3ca697004d18901a466e96
Inosuke <diego-dev1997@hotmail.com>
2021-08-16 23:58:24 -0500

    agregado free shipping y author

--- front/src/components/products/ProductsPageItem.js
+++ front/src/components/products/ProductsPageItem.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import CurrencyFormat from 'react-currency-format';
 import { Link } from 'react-router-dom';
+import shippingImg from '../../assets/img/ic_shipping.png';
 
-export const ProductsPageItem = ({ id, thumbnail, price, title, last = false }) => {
+export const ProductsPageItem = ({ id, thumbnail, price, title, shipping, seller, last = false }) => {
+  const { eshop } = seller;
+  console.log(eshop);
   return (
     <>
       <Link to={`./items/${id}`} className='product__grid' >
@@ -12,10 +15,20 @@ export const ProductsPageItem = ({ id, thumbnail, price, title, last = false }) 
         <div className='product__container__info'>
           <div className='product__container__price'>
             <CurrencyFormat value={price} displayType={'text'} thousandSeparator={true} prefix={'$ '} />
+            {
+              shipping.free_shipping &&
+              <img className='product__image__shipping' src={shippingImg} alt="shippingImg" />
+            }
           </div>
           <div className='product__container__title'>
             {title}
           </div>
+        </div>
+        <div className='product__container__author'>
+          {
+            eshop &&
+            eshop.nick_name
+          }
         </div>
       </Link>
       {
